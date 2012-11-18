b729f305a3d6410180b246e335aa83ed4bcd22cb
jonathan poelen <jonathan.poelen@gmail.com>
2012-11-18 13:32:30 +0100

    simplification of maker<> and create late_maker<>

--- falcon/utility/maker.hpp
+++ falcon/utility/maker.hpp
@@ -5,94 +5,74 @@
 # include <utility>
 # include <falcon/utility/detail/construct_category.hpp>
 #endif
+#include <falcon/c++/constexpr.hpp>
 
 namespace falcon {
 
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
-template<typename _Tp, typename _Tag>
-struct __maker
+template<typename _T, typename _Tag = typename detail::construct_category<_T>::type>
+struct __delegate_maker
 {
-	constexpr __maker()
-	{}
-
-	template<typename _Up, typename _Tag2>
-	constexpr __maker(const __maker<_Up, _Tag2>&)
-	{}
-
-	_Tp operator()() const
-	{
-		return _Tp();
-	}
-
-	_Tp operator()(const _Tp& v) const
-	{
-		return _Tp(v);
-	}
-
 	template<typename... _Args>
-	_Tp operator()(_Args&&... args) const
-	{
-		return _Tp{std::forward<_Args>(args)...};
-	}
+	constexpr static _T __impl(_Args&&... args)
+	{ return _T{std::forward<_Args>(args)...}; }
 };
 
-template<typename _Tp>
-struct __maker<_Tp, ::falcon::detail::initilizer_list_construct_tag>
+template<typename _T>
+struct __delegate_maker<_T, ::falcon::detail::initilizer_list_construct_tag>
 {
-	constexpr __maker()
-	{}
-
-	template<typename _Up, typename _Tag>
-	constexpr __maker(const __maker<_Up, _Tag>&)
-	{}
-
-	_Tp operator()() const
-	{
-		return _Tp();
-	}
-
-	_Tp operator()(const _Tp& v) const
-	{
-		return _Tp(v);
-	}
-
 	template<typename... _Args>
-	_Tp operator()(_Args&&... args) const
-	{
-		return _Tp{{std::forward<_Args>(args)...}};
-	}
+	constexpr static _T __impl(_Args&&... args)
+	{ return _T{{std::forward<_Args>(args)...}}; }
 };
 
 ///wrapper of construct object
-template<typename _Tp>
-struct maker
-: __maker< _Tp, typename ::falcon::detail::construct_category<_Tp>::type>
-{};
+template <template<typename...> class _Template>
+struct late_maker
+{
+	constexpr late_maker()
+	{}
 
-#else
+	template<template<typename...> class _UTemplate>
+	constexpr late_maker(const late_maker<_UTemplate>&)
+	{}
+
+	constexpr _Template<> operator()() const
+	{ return _Template<>(); }
+
+	template<typename... _Args>
+	constexpr _Template<_Args...> operator()(_Args&&... args) const
+	{ return __delegate_maker<_Template<_Args...>>::__impl(args...); }
+};
+#endif
 
 ///wrapper of construct object
 template<typename _Tp>
 struct maker
 {
-	maker()
+	CPP_CONSTEXPR maker()
 	{}
 
 	template<typename _Up>
-	maker(const maker<_Up>&)
+	CPP_CONSTEXPR maker(const maker<_Up>&)
 	{}
 
-	_Tp operator()() const
-	{
-		return _Tp();
-	}
+	CPP_CONSTEXPR _Tp operator()() const
+	{ return _Tp(); }
 
-	_Tp operator()(const _Tp& v) const
-	{
-		return _Tp(v);
-	}
-};
+	CPP_CONSTEXPR _Tp operator()(const _Tp& v) const
+	{ return _Tp(v); }
+
+	template<typename _U>
+	CPP_CONSTEXPR _Tp operator()(const _U& v) const
+	{ return _Tp(v); }
+
+#ifdef __GXX_EXPERIMENTAL_CXX0X__
+	template<typename... _Args>
+	constexpr _Tp operator()(_Args&&... args) const
+	{ return __delegate_maker<_Tp>::__impl(args...); }
 #endif
+};
 
 }
 
