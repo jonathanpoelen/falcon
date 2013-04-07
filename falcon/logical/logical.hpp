37bcd4dfa198351e78b6fd11743ca2757ef6df25
jonathan poelen <jonathan.poelen@gmail.com>
2013-04-07 03:04:51 +0200

    adds constexpr ans creation of not_equal_if_or not_equal_if_and equal_if_or equal_if_and not_equal_if_or_wrapper not_equal_if_and_wrapper equal_if_or_wrapper equal_if_and_wrapper   make_not_equal_if_or make_not_equal_if_and make_equal_if_or make_equal_if_and

--- falcon/logical/logical.hpp
+++ falcon/logical/logical.hpp
@@ -1,8 +1,8 @@
 #ifndef _FALCON_LOGICAL_LOGICAL_HPP
 #define _FALCON_LOGICAL_LOGICAL_HPP
 
-#include <initializer_list>
 #include <type_traits>
+#include <utility>
 
 namespace falcon {
 namespace logical {
@@ -53,6 +53,18 @@ constexpr bool greater_equal_or(const _T& a, const _U& b, const _Args&... args);
 template<typename _T, typename _U, typename... _Args>
 constexpr bool greater_equal_and(const _T& a, const _U& b, const _Args&... args);
 
+template<typename _Predicate, typename _T, typename _U, typename... _Args>
+constexpr bool equal_if_or(_Predicate, const _T& a, const _U& b, const _Args&... args);
+
+template<typename _Predicate, typename _T, typename _U, typename... _Args>
+constexpr bool equal_if_and(_Predicate, const _T& a, const _U& b, const _Args&... args);
+
+template<typename _Predicate, typename _T, typename _U, typename... _Args>
+constexpr bool not_equal_if_or(_Predicate, const _T& a, const _U& b, const _Args&... args);
+
+template<typename _Predicate, typename _T, typename _U, typename... _Args>
+constexpr bool not_equal_if_and(_Predicate, const _T& a, const _U& b, const _Args&... args);
+
 
 template<typename _T, typename _U>
 constexpr bool equal_to_or(const _T& a, const _U& b);
@@ -90,6 +102,18 @@ constexpr bool greater_equal_or(const _T& a, const _U& b);
 template<typename _T, typename _U>
 constexpr bool greater_equal_and(const _T& a, const _U& b);
 
+template<typename _Predicate, typename _T, typename _U>
+constexpr bool equal_if_or(_Predicate, const _T& a, const _U& b);
+
+template<typename _Predicate, typename _T, typename _U>
+constexpr bool equal_if_and(_Predicate, const _T& a, const _U& b);
+
+template<typename _Predicate, typename _T, typename _U>
+constexpr bool not_equal_if_or(_Predicate, const _T& a, const _U& b);
+
+template<typename _Predicate, typename _T, typename _U>
+constexpr bool not_equal_if_and(_Predicate, const _T& a, const _U& b);
+
 
 template <typename _T>
 struct equal_to_or_wrapper;
@@ -126,7 +150,45 @@ struct greater_equal_or_wrapper;
 
 template <typename _T>
 struct greater_equal_and_wrapper;
+
+template <typename _Predicate, typename _T>
+struct equal_if_or_wrapper;
+
+template <typename _Predicate, typename _T>
+struct equal_if_and_wrapper;
+
+template <typename _Predicate, typename _T>
+struct not_equal_if_or_wrapper;
+
+template <typename _Predicate, typename _T>
+struct not_equal_if_and_wrapper;
 //@}
+
+
+template <typename _Predicate, typename _T>
+equal_if_or_wrapper<_Predicate, _T> make_equal_if_or(_Predicate, _T&&);
+
+template <typename _Predicate, typename _T>
+equal_if_or_wrapper<_Predicate, _T> make_equal_if_or(_T&&);
+
+template <typename _Predicate, typename _T>
+equal_if_and_wrapper<_Predicate, _T> make_equal_if_and(_Predicate, _T&&);
+
+template <typename _Predicate, typename _T>
+equal_if_and_wrapper<_Predicate, _T> make_equal_if_and(_T&&);
+
+template <typename _Predicate, typename _T>
+not_equal_if_or_wrapper<_Predicate, _T> make_not_equal_if_or(_Predicate, _T&&);
+
+template <typename _Predicate, typename _T>
+not_equal_if_or_wrapper<_Predicate, _T> make_not_equal_if_or(_T&&);
+
+template <typename _Predicate, typename _T>
+not_equal_if_and_wrapper<_Predicate, _T> make_not_equal_if_and(_Predicate, _T&&);
+
+template <typename _Predicate, typename _T>
+not_equal_if_and_wrapper<_Predicate, _T> make_not_equal_if_and(_T&&);
+
 
 #define __FALCON_MAKE_LOGICAL_FUNC(name, op, op_name)\
 	template<typename _T, typename _U>\
@@ -142,11 +204,11 @@ struct greater_equal_and_wrapper;
 	{\
 		_T _value;\
 		\
-		bool operator()()\
+		constexpr bool operator()() const\
 		{ return false; }\
 		\
 		template <typename... _Args>\
-		bool operator()(const _Args&... args)\
+		constexpr bool operator()(const _Args&... args) const\
 		{ return name##_##op_name<_Args...>(_value, args...); }\
 	};
 
@@ -163,6 +225,56 @@ __FALCON_MAKE2_LOGICAL_FUNC(greater_equal, >=)
 
 #undef __FALCON_MAKE2_LOGICAL_FUNC
 #undef __FALCON_MAKE_LOGICAL_FUNC
+
+#define __FALCON_LOGICAL_NAME(x) x
+#define __FALCON_MAKE_LOGICAL_FUNC(op, prefix_op, _)\
+template<typename _Predicate, typename _T, typename _U>\
+	inline constexpr bool __##prefix_op##_##equal_if_##op(_Predicate & pred, const _T& a, const _U& b)\
+	{ return prefix_op pred(a,b); }\
+	\
+	template<typename _Predicate, typename _T, typename _U, typename... _Args>\
+	constexpr bool __##prefix_op##_##equal_if_##op(_Predicate & pred, const _T& a, const _U& b, const _Args&... args)\
+	{ return (prefix_op pred(a,b)) op __##prefix_op##_##equal_if_##op(a, args...); }\
+	\
+	template<typename _Predicate, typename _T, typename _U>\
+	inline constexpr bool name##_##op(_Predicate pred, const _T& a, const _U& b)\
+	{ return pred(a,b); }\
+	\
+	template<typename _Predicate, typename _T, typename _U, typename... _Args>\
+	constexpr bool name##_##op(_Predicate pred, const _T& a, const _U& b, const _Args&... args)\
+	{ return pred(a,b) op __##prefix_op##_##equal_if_##op(a, args...); }\
+	\
+	template <typename _Predicate, typename _T>\
+	struct __FALCON_LOGICAL_NAME(prefix_op##_##equal_if_##op##_wrapper)\
+	{\
+		_Predicate _pred;\
+		_T _value;\
+	\
+		constexpr bool operator()() const\
+		{ return false; }\
+	\
+		template <typename... _Args>\
+		constexpr bool operator()(const _Args&... args) const\
+		{ return __##prefix_op##_##equal_if_##op<_Args...>(_pred, _value, args...); }\
+	};\
+	\
+	template <typename _Predicate, typename _T>\
+	prefix_op##_##equal_if_##op##_wrapper<_Predicate, _T>\
+	make_##prefix_op##_##equal_if_##op(_Predicate pred, _T&& v)\
+	{ return {pred, std::forward<_T>(v)}; }\
+	\
+	template <typename _Predicate, typename _T>\
+	prefix_op##_##equal_if_##op##_wrapper<_Predicate, _T>\
+	make_##prefix_op##_##equal_if_##op(_T&& v)\
+	{ return {_Predicate(), std::forward<_T>(v)}; }
+
+__FALCON_MAKE_LOGICAL_FUNC(or,,)
+__FALCON_MAKE_LOGICAL_FUNC(and,,)
+__FALCON_MAKE_LOGICAL_FUNC(or, not, _)
+__FALCON_MAKE_LOGICAL_FUNC(and, not, _)
+
+#undef __FALCON_MAKE_LOGICAL_FUNC
+#undef __FALCON_LOGICAL_NAME
 
 }
 }
