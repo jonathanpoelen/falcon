b22e572c90bf8f9a3667fb6ead0f744af6f7c443
jonathan poelen <jonathan.poelen@gmail.com>
2013-06-22 02:08:36 +0200

    indentation

--- falcon/iostream/copy.hpp
+++ falcon/iostream/copy.hpp
@@ -9,6 +9,11 @@ template<typename _CharT, typename _ITraits, typename _OTraits>
 void copy(std::basic_istream<_CharT, _ITraits>& is, std::basic_ios<_CharT, _OTraits>& os)
 { is >> os.rdbuf(); }
 
+template<typename _CharT, typename _ITraits, typename _OTraits>
+void copy(std::basic_istream<_CharT, _ITraits>& is,
+          std::basic_streambuf<_CharT, _OTraits>* dbuf)
+{ is >> dbuf; }
+
 }
 
 #endif
