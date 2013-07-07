8c827ee472d4e65014632a038d40c97c06852c11
jonathan poelen <jonathan.poelen@gmail.com>
2013-07-07 23:57:09 +0200

    xor_swap() and iter_xor_swap()

---
+++ falcon/utility/xor_swap.hpp
@@ -0,0 +1,26 @@
+#ifndef FALCON_UTILITY_XOR_SWAP_HPP
+#define FALCON_UTILITY_XOR_SWAP_HPP
+
+namespace falcon {
+
+template<typename T>
+void xor_swap(T& a, T& b)
+{
+  a ^= b;
+  b ^= a;
+  a ^= b;
+}
+
+template<typename ForwardIt1, typename ForwardIt2>
+void iter_xor_swap(ForwardIt1 a, ForwardIt2 b)
+{
+  for (; a != b; ++a) {
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+  }
+}
+
+}
+
+#endif
