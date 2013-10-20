a05b463c1f89f00624b076618db664a173b1e860
jonathan poelen <jonathan.poelen@gmail.com>
2013-10-20 03:44:59 +0200

    split macro FALCON_CREATE_HAS_MEMBER

--- falcon/sfinae/has_member.hpp
+++ falcon/sfinae/has_member.hpp
@@ -6,12 +6,110 @@
 #include <falcon/c++/boost_or_std.hpp>
 #include FALCON_BOOST_OR_STD_TRAITS(is_class)
 
-#define FALCON_CREATE_HAS_MEMBER_FUNCTION(_Name, _FuncName)\
-	template <typename _Falcon_T,  bool = FALCON_BOOST_OR_STD_NAMESPACE::is_class<_Falcon_T>::value>\
+namespace facon {
+  namespace detail {
+    template <bool, typename R>
+    struct enable_val
+    { typedef R type; };
+  }
+}
+
+#define FALCON_CREATE_HAS_MEMBER_ATTRIBUTE(_Name, _AttrName)\
+  template<typename Falcon_T, class = void>\
+  struct __falcon_##_Name##__has_member_attribute_test\
+  : ::falcon::false_type {};\
+  \
+  template<typename Falcon_T>\
+  struct __falcon_##_Name##__has_member_attribute_test<Falcon_T\
+  , typename ::falcon::detail::enable_val<sizeof(Falcon_T::_AttrName)>::type>\
+  : ::falcon::true_type {};\
+  \
+  template<typename Falcon_T>\
+  struct _Name : __falcon_##_Name##__has_member_attribute_test<Falcon_T>::type {}
+
+
+#define FALCON_CREATE_HAS_FUNCTION(_Name, _AttrName)\
+  template<typename Falcon_T, class = void, class = void>\
+  struct __falcon_##_Name##__has_function_test\
+  : ::falcon::false_type {};\
+  \
+  template<typename Falcon_T>\
+  struct __falcon_##_Name##__has_function_test<Falcon_T\
+  , typename ::falcon::detail::enable_val<sizeof(&Falcon_T::_AttrName)>::type\
+  , typename ::falcon::detail::enable_val<sizeof(Falcon_T::_AttrName)>::type\
+  > : ::falcon::false_type {};\
+  \
+  template<typename Falcon_T, typename Falcon_U>\
+  struct __falcon_##_Name##__has_function_test<Falcon_T\
+  , typename ::falcon::detail::enable_val<sizeof(&Falcon_T::_AttrName)>::type\
+  , Falcon_U> : ::falcon::true_type {};\
+  \
+  template<typename Falcon_T>\
+  struct _Name : __falcon_##_Name##__has_function_test<Falcon_T>::type {}
+
+
+#define FALCON_CREATE_HAS_MEMBER_FUNCTION(_Name, _AttrName)\
+  template<typename Falcon_T, class = void, class = void>\
+  struct __falcon_##_Name##__has_memfunction_test\
+  : ::falcon::false_type {};\
+  \
+  template<typename Falcon_T>\
+  struct __falcon_##_Name##__has_memfunction_test<Falcon_T\
+  , typename ::falcon::detail::enable_val<sizeof(&Falcon_T::_AttrName)>::type\
+  , typename ::falcon::detail::enable_val<sizeof(Falcon_T::_AttrName)>::type\
+  > : ::falcon::false_type {};\
+  \
+  template<typename T, typename U>\
+  struct __falcon_##_Name##__has_memfunction_test<T\
+  , typename std::enable_val<sizeof(&T::xxx)>::type , U>\
+  {\
+    template<typename R, typename TT>\
+    static ::falcon::sfinae_type::one test(R TT::*);\
+    static ::falcon::sfinae_type::two test(...);\
+    static const bool value = sizeof(test(&T::xxx)) == 1;\
+  };\
+  \
+  template<typename Falcon_T>\
+  struct _Name \
+  : ::falcon::integral_constant<bool\
+  , __falcon_##_Name##__has_memfunction_test<Falcon_T>::value\
+  > {}
+
+
+#define FALCON_CREATE_HAS_STATIC_FUNCTION(_Name, _AttrName)\
+  template<typename Falcon_T, class = void, class = void>\
+  struct __falcon_##_Name##__has_sfunction_test\
+  : ::falcon::false_type {};\
+  \
+  template<typename Falcon_T>\
+  struct __falcon_##_Name##__has_sfunction_test<Falcon_T\
+  , typename ::falcon::detail::enable_val<sizeof(&Falcon_T::_AttrName)>::type\
+  , typename ::falcon::detail::enable_val<sizeof(Falcon_T::_AttrName)>::type\
+  > : ::falcon::false_type {};\
+  \
+  template<typename T, typename U>\
+  struct __falcon_##_Name##__has_sfunction_test<T\
+  , typename std::enable_val<sizeof(&T::xxx)>::type , U>\
+  {\
+    template<typename R, typename TT>\
+    static ::falcon::sfinae_type::one test(R TT::*);\
+    static ::falcon::sfinae_type::two test(...);\
+    static const bool value = sizeof(test(&T::xxx)) != 1;\
+  };\
+  \
+  template<typename Falcon_T>\
+  struct _Name \
+  : ::falcon::integral_constant<bool\
+  , __falcon_##_Name##__has_sfunction_test<Falcon_T>::value\
+  > {}
+
+
+#define FALCON_CREATE_HAS_MEMBER(_Name, _FuncName)\
+	template <typename Falcon_T,  bool = FALCON_BOOST_OR_STD_NAMESPACE::is_class<Falcon_T>::value>\
 	class _Name\
 	{\
 		struct base { void _FuncName(); };\
-		struct derived : public _Falcon_T, public base {};\
+		struct derived : public Falcon_T, public base {};\
 		\
 		template <typename U>\
 		static ::falcon::no_type test(U*, ::falcon::sfinae::helper<void(base::*)(), &U::_FuncName>* = 0);\
@@ -19,8 +117,8 @@
 		\
 		FALCON_HELPER_VALUE_TEST(derived);\
 	};\
-	template <typename _Falcon_T>\
-	struct _Name<_Falcon_T, false>\
+	template <typename Falcon_T>\
+	struct _Name<Falcon_T, false>\
 	{ static const bool value = false; }
 
 #define FALCON_HAS_MEMBER_FUNCTION_NAME(_FuncName) has_member_function_##_FuncName
@@ -30,17 +128,17 @@
 
 
 #define FALCON_CREATE_HAS_MEMBER_VAR(_Name, _VarName)\
-	template <typename _Falcon_T,  bool = FALCON_BOOST_OR_STD_NAMESPACE::is_class<_Falcon_T>::value>\
+	template <typename Falcon_T,  bool = FALCON_BOOST_OR_STD_NAMESPACE::is_class<Falcon_T>::value>\
 	class _Name\
 	{\
 		template <typename U, typename V = decltype(U::_VarName)>\
 		static ::falcon::yes_type test(U*);\
 		static ::falcon::no_type test(...);\
 		\
-		FALCON_HELPER_VALUE_TEST(_Falcon_T);\
+		FALCON_HELPER_VALUE_TEST(Falcon_T);\
 	};\
-	template <typename _Falcon_T>\
-	struct _Name<_Falcon_T, false>\
+	template <typename Falcon_T>\
+	struct _Name<Falcon_T, false>\
 	{ static const bool value = false; }
 
 #define FALCON_HAS_MEMBER_VAR_NAME(_VarName) has_member_var_##_VarName
@@ -50,24 +148,24 @@
 
 #if __cplusplus >= 201103L
 # define FALCON_CREATE_HAS_SIGNATURE(_Name, _FuncName)\
-	template <typename _Falcon_T, typename _Signature>\
+	template <typename Falcon_T, typename _Signature>\
 	class _Name;\
 	\
-	template <typename _Falcon_T, typename _Functor, typename... _Args>\
-	class _Name<_Falcon_T, _Functor(_Args...)>\
+	template <typename Falcon_T, typename _Functor, typename... _Args>\
+	class _Name<Falcon_T, _Functor(_Args...)>\
 	{\
 		template <typename T, typename _Member>\
 		struct impl\
 		{\
-			template <typename U> static ::falcon::yes_type test(U*, ::falcon::sfinae::helper<_Member, &U::_FuncName>* = 0);\
+			template <typename U> static ::falcon::yes_type test(U*, ::falcon::sfinae_type::helper<_Member, &U::_FuncName>* = 0);\
 			static ::falcon::no_type test(...);\
-			FALCON_HELPER_VALUE_TEST(_Falcon_T);\
+			FALCON_HELPER_VALUE_TEST(Falcon_T);\
 		};\
 		\
-		typedef typename std::add_const<_Falcon_T>::type _Falcon_T_const;\
+		typedef typename std::add_const<Falcon_T>::type Falcon_T_const;\
 		\
 	public:\
-		static const bool value = impl<_Falcon_T, _Functor(_Falcon_T::*)(_Args...)>::value || impl<_Falcon_T_const, _Functor(_Falcon_T_const::*)(_Args...)>::value;\
+		static const bool value = impl<Falcon_T, _Functor(Falcon_T::*)(_Args...)>::value || impl<Falcon_T_const, _Functor(Falcon_T_const::*)(_Args...)>::value;\
 	}
 
 # define FALCON_HAS_SIGNATURE_NAME(_FuncName) has_##_FuncName##_signature
--- falcon/sfinae/has_type.hpp
+++ falcon/sfinae/has_type.hpp
@@ -2,7 +2,6 @@
 #define FALCON_SFINAE_HAS_TYPE_HPP
 
 #include <falcon/type_traits/integral_constant.hpp>
-#include <falcon/type_traits/enable_if_type.hpp>
 #include <falcon/type_traits/yes_no_type.hpp>
 
 #define __FALCON_HELPER_VALUE_TEST(_T)\
@@ -19,20 +18,74 @@
 #define FALCON_HELPER_VALUE_TEST(_T) __FALCON_HELPER_VALUE_TEST(_T)
 #endif
 
+namespace falcon {
+  namespace detail {
+    template<class T, class R = void>
+    struct enable_type
+    { typedef R type; };
+
+#if __cplusplus >= 201103L
+    template<template<typename...>class T, class R = void>
+    struct enable_template
+    { typedef R type; };
+#else
+    template<template<typename>class T, class R = void>
+    struct enable_template1
+    { typedef R type; };
+
+    template<template<typename,typename>class T, class R = void>
+    struct enable_template2
+    { typedef R type; };
+#endif
+  }
+}
+
 #define FALCON_CREATE_HAS_TYPE(_Name, _TypeName)\
-  template<typename _Falcon_T, class = void>\
-  struct __falcon_##_Name##__test : false_type {};\
+  template<typename Falcon_T, class = void>\
+  struct __falcon_##_Name##__has_type_test\
+  : ::falcon::false_type {};\
   \
-  template<typename _Falcon_T>\
-  struct __falcon_##_Name##__test<_Falcon_T\
-  , typename enable_if_type<typename _Falcon_T::_TypeName>::type> : true_type {};\
+  template<typename Falcon_T>\
+  struct __falcon_##_Name##__has_type_test<Falcon_T\
+  , typename ::falcon::enable_type<typename Falcon_T::_TypeName>::type>\
+  : ::falcon::true_type {};\
   \
-  template<typename _Falcon_T>\
-  struct _Name : __falcon_##_Name##__test<_Falcon_T>::type {}
+  template<typename Falcon_T>\
+  struct _Name : __falcon_##_Name##__has_type_test<Falcon_T>::type {}
 
 #define FALCON_HAS_TYPE_NAME(_TypeName) has_##_TypeName
 
 #define FALCON_CLASS_HAS_TYPE(_TypeName)\
 	FALCON_CREATE_HAS_TYPE(has_##_TypeName, _TypeName)
+
+
+#if __cplusplus >= 201103L
+# define __FALCON_FALCON_CREATE_HAS_TEMPLATE_TYPE(_Name, _TypeName)\
+  template<typename Falcon_T>\
+  struct __falcon_##_Name##__has_template_type_test<Falcon_T\
+  , typename ::falcon::detail::enable_template<Falcon_T::template xxx>::type>\
+  : ::falcon::true_type {};
+#else
+# define __FALCON_FALCON_CREATE_HAS_TEMPLATE_TYPE(_Name, _TypeName)\
+  template<typename Falcon_T>\
+  struct __falcon_##_Name##__has_template_type_test<Falcon_T\
+  , typename ::falcon::detail::enable_template<Falcon_T::template xxx>::type>\
+  : ::falcon::true_type {};\
+  \
+  template<typename Falcon_T>\
+  struct __falcon_##_Name##__has_template_type_test<Falcon_T\
+  , typename ::falcon::detail::enable_template<Falcon_T::template xxx>::type>\
+  : ::falcon::true_type {};
+#endif
+
+#define FALCON_CREATE_HAS_TEMPLATE_TYPE(_Name, _TypeName)\
+	template<typename Falcon_T, class = void>\
+	struct __falcon_##_Name##__has_template_type_test\
+	: ::falcon::false_type {};\
+  \
+  __FALCON_FALCON_CREATE_HAS_TEMPLATE_TYPE(_Name, _TypeName)\
+  \
+  template<typename Falcon_T>\
+  struct _Name : __falcon_##_Name##__has_template_type_test<Falcon_T>::type {}
 
 #endif
