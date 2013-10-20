7dd2997981d7042397469503c57075a60b2b181b
jonathan poelen <jonathan.poelen@gmail.com>
2013-10-20 23:26:20 +0200

    new strategy for function_is_call_possible.hpp

---
+++ falcon/sfinae/function_is_callable.hpp
@@ -0,0 +1,151 @@
+#ifndef FALCON_SFINAE_FUNCTION_IS_CALLABLE_HPP
+#define FALCON_SFINAE_FUNCTION_IS_CALLABLE_HPP
+
+#include <falcon/sfinae/detail/is_callable.hpp>
+#if __cplusplus >= 201103L
+# include <falcon/parameter/parameter_pack.hpp>
+# include <type_traits>
+#else
+# include <falcon/utility/declval.hpp>
+#endif
+
+
+#if __cplusplus >= 201103L
+
+# define FALCON_CREATE_FUNCTION_IS_CALLABLE(_Name, _FuncName)\
+  template<typename, typename = void>\
+  struct __falcon_##_Name##__is_callable_test\
+  : ::falcon::false_type {};\
+  \
+  template<typename... Falcon_Args>\
+  struct __falcon_##_Name##__is_callable_test<::falcon::parameter_pack<Falcon_Args...>\
+  , typename ::falcon::detail::enable_val<\
+    sizeof(_FuncName(::std::declval<Falcon_Args>()...),0)\
+  >::type> : ::falcon::true_type {};\
+  \
+  template<typename... Falcon_Args>\
+  struct _Name : __falcon_##_Name##__is_callable_test<\
+    ::falcon::parameter_pack<Falcon_Args...>>::type {};
+
+
+# define FALCON_CREATE_FUNCTION_IS_CALLABLE_AND_CONVERTIBLE(_Name, _FuncName)\
+  template<typename, typename, typename = void>\
+  struct __falcon_##_Name##__is_callable_and_conv_test\
+  : ::falcon::false_type {};\
+  \
+  template<typename Falcon_Result, typename... Falcon_Args>\
+  struct __falcon_##_Name##__is_callable_and_conv_test<Falcon_Result\
+  , ::falcon::parameter_pack<Falcon_Args...>\
+  , typename ::falcon::detail::enable_val<\
+    sizeof(returnval<Falcon_Result>(_FuncName(::std::declval<Args>()...)))\
+  >::type> : ::falcon::true_type {};\
+  \
+  template<typename... Falcon_Args>\
+  struct __falcon_##_Name##__is_callable_and_conv_test<void\
+  , ::falcon::parameter_pack<Falcon_Args...>\
+  , typename ::falcon::detail::enable_val<\
+    sizeof(_FuncName(::std::declval<Args>()...),0)\
+  >::type> : ::falcon::true_type {};\
+  \
+  template<typename Falcon_Result, typename... Falcon_Args>\
+  struct _Name : __falcon_##_Name##__is_callable_and_conv_test<Falcon_Result\
+  , ::falcon::parameter_pack<Falcon_Args...>>::type {};
+
+#else
+
+# define FALCON_CREATE_FUNCTION_IS_CALLABLE(_Name, _FuncName)\
+  template<typename, typename, typename = void>\
+  struct __falcon_##_Name##__is_callable_test\
+  : ::falcon::false_type {};\
+  \
+  template<>\
+  struct __falcon_##_Name##__is_callable_test<\
+    ::falcon::detail::sfinae_unspecified, ::falcon::detail::sfinae_unspecified\
+  , typename ::falcon::detail::enable_val<sizeof(_FuncName(),0)>::type\
+  > : ::falcon::true_type {};\
+  \
+  template<typename Falcon_U>\
+  struct __falcon_##_Name##__is_callable_test<Falcon_U\
+  , ::falcon::detail::sfinae_unspecified\
+  , typename ::falcon::detail::enable_val<\
+    sizeof(_FuncName(::falcon::declval<Falcon_U>()),0)\
+  >::type> : ::falcon::true_type {};\
+  \
+  template<typename Falcon_U, typename Falcon_U>\
+  struct __falcon_##_Name##__is_callable_test<Falcon_U, Falcon_U\
+  , typename ::falcon::detail::enable_val<\
+    sizeof(_FuncName(::falcon::declval<Falcon_U>()\
+    , ::falcon::declval<Falcon_U>()),0)\
+  >::type> : ::falcon::true_type {};\
+  \
+  template<\
+    typename Falcon_U = ::falcon::detail::sfinae_unspecified\
+  , typename Falcon_U = ::falcon::detail::sfinae_unspecified>\
+  struct _Name : __falcon_##_Name##__is_callable_test<Falcon_U, Falcon_U>::type {};
+
+
+# define FALCON_CREATE_FUNCTION_IS_CALLABLE_AND_CONVERTIBLE(_Name, _FuncName)\
+  template<typename, typename, typename, typename = void>\
+  struct __falcon_##_Name##__is_callable_test\
+  : ::falcon::false_type {};\
+  \
+  template<>\
+  struct __falcon_##_Name##__is_callable_test<void\
+  , ::falcon::detail::sfinae_unspecified, ::falcon::detail::sfinae_unspecified\
+  , typename ::falcon::detail::enable_val<sizeof(_FuncName(),0)>::type\
+  > : ::falcon::true_type {};\
+  \
+  template<typename Falcon_Result>\
+  struct __falcon_##_Name##__is_callable_test<Falcon_Result\
+  , ::falcon::detail::sfinae_unspecified, ::falcon::detail::sfinae_unspecified\
+  , typename ::falcon::detail::enable_val<\
+    sizeof(returnval<Falcon_Result>(_FuncName()))\
+  >::type> : ::falcon::true_type {};\
+  \
+  template<typename Falcon_U>\
+  struct __falcon_##_Name##__is_callable_test<void, Falcon_U\
+  , ::falcon::detail::sfinae_unspecified\
+  , typename ::falcon::detail::enable_val<\
+    sizeof(_FuncName(::falcon::declval<Falcon_U>()),0)\
+  >::type> : ::falcon::true_type {};\
+  \
+  template<typename Falcon_Result, typename Falcon_U>\
+  struct __falcon_##_Name##__is_callable_test<Falcon_Result, Falcon_U\
+  , ::falcon::detail::sfinae_unspecified\
+  , typename ::falcon::detail::enable_val<\
+    sizeof(returnval<Falcon_Result>(\
+      _FuncName(::falcon::declval<Falcon_U>())))\
+  >::type> : ::falcon::true_type {};\
+  \
+  template<typename Falcon_U, typename Falcon_U>\
+  struct __falcon_##_Name##__is_callable_test<void, Falcon_U, Falcon_U\
+  , typename ::falcon::detail::enable_val<\
+    sizeof(_FuncName(::falcon::declval<Falcon_U>()\
+    , ::falcon::declval<Falcon_U>()),0)\
+  >::type> : ::falcon::true_type {};\
+  \
+  template<typename Falcon_Result, typename Falcon_U, typename Falcon_U>\
+  struct __falcon_##_Name##__is_callable_test<Falcon_Result, Falcon_U, Falcon_U\
+  , typename ::falcon::detail::enable_val<\
+    sizeof(returnval<Falcon_Result>(\
+      _FuncName(::falcon::declval<Falcon_U>()\
+    , ::falcon::declval<Falcon_U>())))\
+  >::type> : ::falcon::true_type {};\
+  \
+  template<typename Falcon_Result\
+  , typename Falcon_U = ::falcon::detail::sfinae_unspecified\
+  , typename Falcon_U = ::falcon::detail::sfinae_unspecified>\
+  struct _Name : __falcon_##_Name##__is_callable_test<\
+    Falcon_Result, Falcon_U, Falcon_U>::type {};
+
+#endif
+
+
+#define FALCON_CLASS_FUNCTION_IS_CALLABLE(_FuncName)\
+  FALCON_CREATE_FUNCTION_IS_CALLABLE(_FuncName##_is_callable, _FuncName)
+
+#define FALCON_CLASS_FUNCTION_IS_CALLABLE_AND_CONVERTIBLE(_FuncName)\
+  FALCON_CREATE_FUNCTION_IS_CALLABLE_AND_CONVERTIBLE(\
+    _FuncName##_member_is_callalable_and_convertible, _FuncName)
+
+#endif
--- falcon/sfinae/member_is_callable.hpp
+++ falcon/sfinae/member_is_callable.hpp
@@ -1,27 +1,14 @@
 #ifndef FALCON_SFINAE_MEMBER_IS_CALLABLE_HPP
 #define FALCON_SFINAE_MEMBER_IS_CALLABLE_HPP
 
+#include <falcon/sfinae/detail/is_callable.hpp>
 #if __cplusplus >= 201103L
 # include <falcon/parameter/parameter_pack.hpp>
 # include <type_traits>
 #else
 # include <falcon/utility/declval.hpp>
-# include <falcon/type_traits/enable_if.hpp>
-# include <boost/type_traits/is_convertible.hpp>
 #endif
 
-namespace falcon {
-  namespace detail {
-    template<typename T, typename U>
-#if __cplusplus >= 201103L
-    typename std::enable_if<std::is_convertible<U,T>::value, T>::type
-    returnval(U&&);
-#else
-    typename falcon::enable_if<boost::is_convertible<U,T>::value, T>::type
-    returnval(const U&);
-#endif
-  }
-}
 
 #if __cplusplus >= 201103L
 
@@ -66,11 +53,6 @@ namespace falcon {
 
 #else
 
-namespace falcon {
-  namespace detail {
-    class sfinae_unspecified;
-  }
-}
 # define FALCON_CREATE_MEMBER_FUNCTION_IS_CALLABLE(_Name, _FuncName)\
   template<typename Falcon_T, typename, typename, typename = void>\
   struct __falcon_##_Name##__is_callable_test\
@@ -89,17 +71,17 @@ namespace falcon {
     sizeof(::falcon::declval<Falcon_T>()._FuncName(::falcon::declval<Falcon_U>()),0)\
   >::type> : ::falcon::true_type {};\
   \
-  template<typename Falcon_T, typename Falcon_U, typename Falcon_UFalcon_U>\
-  struct __falcon_##_Name##__is_callable_test<Falcon_T, Falcon_U, Falcon_UFalcon_U\
+  template<typename Falcon_T, typename Falcon_U, typename Falcon_U>\
+  struct __falcon_##_Name##__is_callable_test<Falcon_T, Falcon_U, Falcon_U\
   , typename ::falcon::detail::enable_val<\
     sizeof(::falcon::declval<Falcon_T>()._FuncName(::falcon::declval<Falcon_U>()\
-    , ::falcon::declval<Falcon_UFalcon_U>()),0)\
+    , ::falcon::declval<Falcon_U>()),0)\
   >::type> : ::falcon::true_type {};\
   \
   template<typename Falcon_T\
   , typename Falcon_U = ::falcon::detail::sfinae_unspecified\
-  , typename Falcon_UFalcon_U = ::falcon::detail::sfinae_unspecified>\
-  struct _Name : __falcon_##_Name##__is_callable_test<Falcon_T, Falcon_U, Falcon_UFalcon_U>::type {};
+  , typename Falcon_U = ::falcon::detail::sfinae_unspecified>\
+  struct _Name : __falcon_##_Name##__is_callable_test<Falcon_T, Falcon_U, Falcon_U>::type {};
 
 
 # define FALCON_CREATE_MEMBER_FUNCTION_IS_CALLABLE_AND_CONVERTIBLE(_Name, _FuncName)\
@@ -135,26 +117,26 @@ namespace falcon {
       ::falcon::declval<Falcon_T>()._FuncName(::falcon::declval<Falcon_U>())))\
   >::type> : ::falcon::true_type {};\
   \
-  template<typename Falcon_T, typename Falcon_U, typename Falcon_UFalcon_U>\
-  struct __falcon_##_Name##__is_callable_test<Falcon_T, void, Falcon_U, Falcon_UFalcon_U\
+  template<typename Falcon_T, typename Falcon_U, typename Falcon_U>\
+  struct __falcon_##_Name##__is_callable_test<Falcon_T, void, Falcon_U, Falcon_U\
   , typename ::falcon::detail::enable_val<\
     sizeof(::falcon::declval<Falcon_T>()._FuncName(::falcon::declval<Falcon_U>()\
-    , ::falcon::declval<Falcon_UFalcon_U>()),0)\
+    , ::falcon::declval<Falcon_U>()),0)\
   >::type> : ::falcon::true_type {};\
   \
-  template<typename Falcon_T, typename Falcon_Result, typename Falcon_U, typename Falcon_UFalcon_U>\
-  struct __falcon_##_Name##__is_callable_test<Falcon_T, Falcon_Result, Falcon_U, Falcon_UFalcon_U\
+  template<typename Falcon_T, typename Falcon_Result, typename Falcon_U, typename Falcon_U>\
+  struct __falcon_##_Name##__is_callable_test<Falcon_T, Falcon_Result, Falcon_U, Falcon_U\
   , typename ::falcon::detail::enable_val<\
     sizeof(returnval<Falcon_Result>(\
       ::falcon::declval<Falcon_T>()._FuncName(::falcon::declval<Falcon_U>()\
-    , ::falcon::declval<Falcon_UFalcon_U>())))\
+    , ::falcon::declval<Falcon_U>())))\
   >::type> : ::falcon::true_type {};\
   \
   template<typename Falcon_T, typename Falcon_Result\
   , typename Falcon_U = ::falcon::detail::sfinae_unspecified\
-  , typename Falcon_UFalcon_U = ::falcon::detail::sfinae_unspecified>\
+  , typename Falcon_U = ::falcon::detail::sfinae_unspecified>\
   struct _Name : __falcon_##_Name##__is_callable_test<Falcon_T\
-  , Falcon_Result, Falcon_U, Falcon_UFalcon_U>::type {};
+  , Falcon_Result, Falcon_U, Falcon_U>::type {};
 
 #endif
 
