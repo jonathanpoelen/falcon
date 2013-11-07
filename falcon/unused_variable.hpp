f05b01f382fcbb33cdf3e3f244fa4aa520f9a97a
jonathan poelen <jonathan.poelen@gmail.com>
2013-11-07 09:48:29 +0100

    clean code

--- falcon/unused_variable.hpp
+++ falcon/unused_variable.hpp
@@ -2,26 +2,24 @@
 #define FALCON_UNUSED_VARIABLE_HPP
 
 #include <falcon/c++/constexpr.hpp>
-#include <falcon/preprocessor/not_ide_parser.hpp>
 
 namespace falcon {
 #if __cplusplus >= 201103L
-template<typename... _Args>
-//void unused_variables(const _Args&...) = kdevelop parser error -_-
-constexpr inline void unused_variables(const _Args&... FALCON_PP_NOT_IDE_PARSER())
+template<typename... Args>
+constexpr inline void unused_variables(const Args&...)
 {}
 #else
 inline void unused_variables(...)
 {}
 #endif
 
-template<typename _T>
-CPP_CONSTEXPR inline void unused_variable(const _T&)
+template<typename T>
+CPP_CONSTEXPR inline void unused_variable(const T&)
 {}
 
 }
 
-#define FALCON_UNUSED(args...) ::falcon::unused_variables(args)
+#define FALCON_UNUSED(...) ::falcon::unused_variables(__VA_ARGS__)
 
 #if __cplusplus >= 201103L
 # define FALCON_UNUSED_PACK(pack) (void)sizeof...(pack)
