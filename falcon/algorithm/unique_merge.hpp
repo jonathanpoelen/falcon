b8caf450ba5de6b9e7ab9faf513d75f3e03f382f
jonathan poelen <jonathan.poelen@gmail.com>
2014-01-11 19:22:41 +0100

    unique_merge

---
+++ falcon/algorithm/unique_merge.hpp
@@ -0,0 +1,130 @@
+#ifndef FALCON_ALGORITHM_MERGE_UNIQUE_HPP
+#define FALCON_ALGORITHM_MERGE_UNIQUE_HPP
+
+#include <falcon/functional/operators.hpp>
+#include <iterator>
+#include <algorithm>
+
+namespace falcon {
+
+template<class ForwardIt, class OutputIt, class BinaryPredicate>
+OutputIt __unique_merge(ForwardIt first1, ForwardIt last1,
+                        ForwardIt first2, ForwardIt last2,
+                        OutputIt d_first,
+                        BinaryPredicate binary_pred,
+                        std::forward_iterator_tag)
+{
+  if (first1 != last1 && first2 != last2) {
+    ForwardIt x = (*first1 < *first2) ? first1++ : first2++;
+    *d_first = *x;
+    ++d_first;
+
+    while (first1 != last1) {
+      if (first2 == last2) {
+        while (first1 != last1 && *x == *first1) {
+          ++first1;
+        }
+        d_first = std::unique_copy(first1, last1, d_first);
+        break ;
+      }
+      if (*first2 < *first1) {
+        if (!bool(binary_pred(*x, *first2))) {
+          x = first2;
+          *d_first = *x;
+          ++d_first;
+        }
+        ++first2;
+      } else {
+        if (!bool(binary_pred(*x, *first1))) {
+          x = first1;
+          *d_first = *x;
+          ++d_first;
+        }
+        ++first1;
+      }
+    }
+
+    while (first2 != last2 && *x == *first2) {
+      ++first2;
+    }
+  }
+  else if (first1 != last1) {
+    return std::unique_copy(first1, last1, d_first);
+  }
+
+  return std::unique_copy(first2, last2, d_first);
+}
+
+template<class InputIt, class InputIt2, class OutputIt, class BinaryPredicate>
+OutputIt __unique_merge(InputIt first1, InputIt last1,
+                        InputIt2 first2, InputIt2 last2,
+                        OutputIt d_first,
+                        BinaryPredicate binary_pred,
+                        std::input_iterator_tag)
+{
+  if (first1 != last1 && first2 != last2) {
+    typedef typename std::iterator_traits<InputIt>::value_type value_type;
+    value_type x = (*first1 < *first2) ? *first1++ : *first2++;
+    *d_first = x;
+    ++d_first;
+
+    while (first1 != last1) {
+      if (first2 == last2) {
+        while (first1 != last1 && x == *first1) {
+          ++first1;
+        }
+        d_first = std::unique_copy(first1, last1, d_first);
+        break ;
+      }
+      if (*first2 < *first1) {
+        if (!bool(binary_pred(x, *first2))) {
+          x = *first2;
+          *d_first = x;
+          ++d_first;
+        }
+        ++first2;
+      } else {
+        if (!bool(binary_pred(x, *first1))) {
+          x = *first1;
+          *d_first = x;
+          ++d_first;
+        }
+        ++first1;
+      }
+    }
+
+    while (first2 != last2 && binary_pred(x, *first2)) {
+      ++first2;
+    }
+  }
+  else if (first1 != last1) {
+    return std::unique_copy(first1, last1, d_first);
+  }
+
+  return std::unique_copy(first2, last2, d_first);
+}
+
+
+template<class InputIt1, class InputIt2, class OutputIt>
+OutputIt unique_merge(InputIt1 first1, InputIt1 last1,
+                      InputIt2 first2, InputIt2 last2,
+                      OutputIt d_first)
+{
+  return __unique_merge(first1, last1, first2, last2, d_first, late_equal_to(),
+                        std::iterator_traits<InputIt1>::iterator_category());
+}
+
+
+template<class InputIt1, class InputIt2, class OutputIt, class BinaryPredicate>
+OutputIt unique_merge(InputIt1 first1, InputIt1 last1,
+                      InputIt2 first2, InputIt2 last2,
+                      OutputIt d_first,
+                      BinaryPredicate binary_pred)
+{
+  return __unique_merge(first1, last1, first2, last2, d_first, binary_pred,
+                        std::iterator_traits<InputIt1>::iterator_category());
+}
+
+}
+
+#endif
