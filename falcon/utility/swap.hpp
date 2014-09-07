337e3f7ac14466a2d3a9c12513552fb4e8713c8d
jonathan poelen <jonathan.poelen@gmail.com>
2014-09-07 14:17:44 +0200

    noexcept specifier for swap

--- falcon/utility/swap.hpp
+++ falcon/utility/swap.hpp
@@ -6,28 +6,33 @@
 #else
 # include <algorithm>
 #endif
+#include <falcon/c++/noexcept.hpp>
+
+namespace falcon_swap_impl {
+  using std::swap;
+
+  template<class T>
+  void swap_impl(T & a, T & b) CPP_NOEXCEPT_OPERATOR2(swap(a,b)) {
+    swap(a,b);
+  }
+}
 
 namespace falcon {
 
 template<typename T>
 void swap(T& x, T& y)
-{
-  using std::swap;
-  swap(x,y);
-}
+CPP_NOEXCEPT_OPERATOR2(::falcon_swap_impl::swap_impl(x,y))
+{ ::falcon_swap_impl::swap_impl(x,y); }
 
+#if __cplusplus < 201103L
 template<typename T, std::size_t N>
 void swap(T(&x)[N], T(&y)[N])
 {
-  using std::swap;
-#if __cplusplus >= 201103L
-  swap(x,y);
-#else
   for (std::size_t i = 0; i < N; ++i) {
-    swap(x[i], y[i]);
+    ::falcon_swap_impl::swap_impl(x[i], y[i]);
   }
-#endif
 }
+#endif
 
 }
 
