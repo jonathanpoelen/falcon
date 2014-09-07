d30dc9b6834c1cf15a0229defa46f154a7273794
jonathan poelen <jonathan.poelen@gmail.com>
2014-09-08 00:33:09 +0200

    FALCON_HAS_TEMPLATE_TYPE_NAME() and code cleaning

--- falcon/sfinae/has_type.hpp
+++ falcon/sfinae/has_type.hpp
@@ -4,63 +4,62 @@
 #include <falcon/type_traits/integral_constant.hpp>
 #include <falcon/type_traits/enable_template.hpp>
 #include <falcon/type_traits/enable_type.hpp>
+#include <falcon/preprocessor/concat.hpp>
 
 
-#define FALCON_HAS_TYPE_TRAIT_NAMED_DEF(_Name, _TypeName)\
+#define FALCON_HAS_TYPE_TRAIT_NAMED_DEF(trait, name)\
   template<typename Falcon_T, class = void>\
-  struct __falcon_##_Name##__has_type_test\
+  struct FALCON_CONCAT(trait,_impl)\
   : ::falcon::false_type {};\
   \
   template<typename Falcon_T>\
-  struct __falcon_##_Name##__has_type_test<Falcon_T\
-  , typename ::falcon::enable_type<typename Falcon_T::_TypeName>::type>\
+  struct FALCON_CONCAT(trait,_impl)<Falcon_T\
+  , typename ::falcon::enable_type<typename Falcon_T::name>::type>\
   : ::falcon::true_type {};\
   \
   template<typename Falcon_T>\
-  struct _Name : __falcon_##_Name##__has_type_test<Falcon_T>::type {\
-    static const bool value = __falcon_##_Name##__has_type_test<Falcon_T>::value;\
-  }
-
-#define FALCON_HAS_TYPE_NAME(_TypeName) has_##_TypeName
+  struct trait : FALCON_CONCAT(trait,_impl)<Falcon_T>::type {}
 
 
 #if __cplusplus >= 201103L
-# define __FALCON_HAS_TEMPLATE_TYPE_TRAIT_NAMED_DEF(_Name, _TypeName)\
+# define FALCON_HAS_TEMPLATE_TYPE_TRAIT_NAMED_DEF_S(trait, name)\
   template<typename Falcon_T>\
-  struct __falcon_##_Name##__has_template_type_test<Falcon_T\
-  , typename ::falcon::enable_template<Falcon_T::template _TypeName>::type>\
+  struct FALCON_CONCAT(trait,_impl)<Falcon_T\
+  , typename ::falcon::enable_template<Falcon_T::template name>::type>\
   : ::falcon::true_type {};
 #else
-# define __FALCON_HAS_TEMPLATE_TYPE_TRAIT_NAMED_DEF(_Name, _TypeName)\
+# define FALCON_HAS_TEMPLATE_TYPE_TRAIT_NAMED_DEF_S(trait, name)\
   template<typename Falcon_T>\
-  struct __falcon_##_Name##__has_template_type_test<Falcon_T\
-  , typename ::falcon::enable_template1<Falcon_T::template _TypeName>::type>\
+  struct FALCON_CONCAT(trait,_impl)<Falcon_T\
+  , typename ::falcon::enable_template1<Falcon_T::template name>::type>\
   : ::falcon::true_type {};\
   \
   template<typename Falcon_T>\
-  struct __falcon_##_Name##__has_template_type_test<Falcon_T\
-  , typename ::falcon::enable_template2<Falcon_T::template _TypeName>::type>\
+  struct FALCON_CONCAT(trait,_impl)<Falcon_T\
+  , typename ::falcon::enable_template2<Falcon_T::template name>::type>\
   : ::falcon::true_type {};
 #endif
 
-#define FALCON_HAS_TEMPLATE_TYPE_TRAIT_NAMED_DEF(_Name, _TypeName)\
+#define FALCON_HAS_TEMPLATE_TYPE_TRAIT_NAMED_DEF(trait, name)\
 	template<typename Falcon_T, class = void>\
-	struct __falcon_##_Name##__has_template_type_test\
+	struct FALCON_CONCAT(trait,_impl)\
 	: ::falcon::false_type {};\
   \
-  __FALCON_HAS_TEMPLATE_TYPE_TRAIT_NAMED_DEF(_Name, _TypeName)\
+  FALCON_HAS_TEMPLATE_TYPE_TRAIT_NAMED_DEF_S(trait, name)\
   \
   template<typename Falcon_T>\
-  struct _Name : __falcon_##_Name##__has_template_type_test<Falcon_T>::type {\
-    const static bool value = \
-      __falcon_##_Name##__has_template_type_test<Falcon_T>::value;\
-  }
+  struct trait : FALCON_CONCAT(trait,_impl)<Falcon_T>::type {}
 
 
-#define FALCON_HAS_TYPE_TRAIT_DEF(_TypeName)\
-  FALCON_HAS_TYPE_TRAIT_NAMED_DEF(has_##_TypeName, _TypeName)
+#define FALCON_HAS_TYPE_NAME(name) FALCON_CONCAT(has_,name)
 
-#define FALCON_HAS_TEMPLATE_TYPE_TRAIT_DEF(_TypeName)\
-  FALCON_HAS_TEMPLATE_TYPE_TRAIT_NAMED_DEF(has_template_##_TypeName, _TypeName)
+#define FALCON_HAS_TYPE_TRAIT_DEF(name)\
+  FALCON_HAS_TYPE_TRAIT_NAMED_DEF(FALCON_HAS_TYPE_NAME(name), name)
+
+
+#define FALCON_HAS_TEMPLATE_TYPE_NAME(name) FALCON_CONCAT(has_template_,name)
+
+#define FALCON_HAS_TEMPLATE_TYPE_TRAIT_DEF(name)\
+  FALCON_HAS_TEMPLATE_TYPE_TRAIT_NAMED_DEF(FALCON_HAS_TEMPLATE_TYPE_NAME(name), name)
 
 #endif
