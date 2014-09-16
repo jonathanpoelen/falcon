c71b71576d6cfacc337f1e8110342939fee114ca
jonathan poelen <jonathan.poelen@gmail.com>
2014-09-16 23:42:15 +0200

    new interface for sto* and removing detail::xstring

--- falcon/string/stox.hpp
+++ falcon/string/stox.hpp
@@ -7,109 +7,84 @@
 #include <limits>
 #include <stdexcept>
 #include <cerrno>
-#include <cstdarg>
-#if __cplusplus < 201103L
-# if defined __USE_GNU || defined __USE_BSD || defined __USE_MISC
-#   include <alloca.h>
-# endif
-#endif
 
 namespace falcon {
 
-namespace detail {
+namespace aux_ {
+
 template<typename TRet, typename Ret, typename CharT>
-Ret __stoa_aux(const TRet& __tmp, Ret& __ret, const char* __name,
-               const CharT* __str, std::size_t* __idx, CharT* __endptr)
+void stoa_aux(const TRet tmp, Ret& ret, const char* name,
+              const CharT* str, std::size_t* idx, CharT* endptr)
 {
-  if (__endptr == __str)
-    throw std::invalid_argument(__name);
+  if (endptr == str)
+    throw std::invalid_argument(name);
   else if (errno == ERANGE
     || (falcon::is_same<Ret, int>::value
-    && (__tmp < static_cast<TRet>(std::numeric_limits<int>::min())
-    || __tmp > static_cast<TRet>(std::numeric_limits<int>::max()))))
-    throw std::out_of_range(__name);
+      && (tmp < static_cast<TRet>(std::numeric_limits<int>::min())
+      || tmp > static_cast<TRet>(std::numeric_limits<int>::max()))))
+    throw std::out_of_range(name);
   else
-    __ret = static_cast<Ret>(__tmp);
+    ret = static_cast<Ret>(tmp);
 
-  if (__idx)
-    *__idx = __endptr - __str;
+  if (idx)
+    *idx = std::size_t(endptr - str);
 }
 
 // Helper for all the sto* functions.
-template<typename Ret, typename TRet, typename CharT>
-Ret stoa(TRet (*__convf) (const CharT*, CharT**),
-         const char* __name, const CharT* __str, std::size_t* __idx)
+template<typename Ret, typename Conv, typename CharT>
+Ret stoa(Conv conv, const char* name, const CharT* str, std::size_t* idx)
 {
-  Ret __ret;
-
-  CharT* __endptr;
+  Ret ret;
+  CharT* endptr;
   errno = 0;
-  const TRet __tmp = __convf(__str, &__endptr);
-  __stoa_aux(__tmp, __ret, __name, __str, __idx, __endptr);
-  return __ret;
+  stoa_aux(conv(str, &endptr), ret, name, str, idx, endptr);
+  return ret;
 }
 
-template<typename Ret, typename TRet, typename CharT>
-Ret stoa(TRet (*__convf) (const CharT*, CharT**, int base),
-         const char* __name, const CharT* __str, std::size_t* __idx,
-         int __base)
+template<typename Ret, typename Conv, typename CharT>
+Ret stoa(Conv conv, const char* name, const CharT* str, std::size_t* idx, int base)
 {
-  Ret __ret;
-
-  CharT* __endptr;
+  Ret ret;
+  CharT* endptr;
   errno = 0;
-  const TRet __tmp = __convf(__str, &__endptr, __base);
-  __stoa_aux(__tmp, __ret, __name, __str, __idx, __endptr);
-  return __ret;
-}
-
-// Helper for the to_string / to_wstring functions.
-template<typename String, typename CharT>
-String to_xstring(int (*__convf) (CharT*, std::size_t, const CharT*, std::va_list),
-                  std::size_t __n, const CharT* __fmt, ...)
-{
-#if __cplusplus >= 201103L
-  String ret;
-  ret.resize(__n);
-  CharT* __s = &ret[0];
-#elif defined __USE_GNU || defined __USE_BSD || defined __USE_MISC
-  CharT* __s = static_cast<CharT*>(alloca(sizeof(CharT) * __n));
-#else
-  CharT* __s = new CharT[__n];
-#endif
-
-  std::va_list __args;
-  va_start(__args, __fmt);
-
-  const int __len = __convf(__s, __n, __fmt, __args);
-
-  va_end(__args);
-
-#if __cplusplus >= 201103L
-  (void)__len;
+  stoa_aux(conv(str, &endptr, base), ret, name, str, idx, endptr);
   return ret;
-#elif defined __USE_GNU || defined __USE_BSD || defined __USE_MISC
-  return String(__s, __s + __len);
-# else
-  String ret(__s, __s + __len);
-  delete [] __s;
-  return ret;
-#endif
-}
 }
 
-#define FALCON_BASIC_CSTRING_TO_IMPL(result_type, fname, std_fname)            \
-  template<typename CharT, typename Allocator>                                 \
+}
+
+#define FALCON_BASIC_CSTRING_TO_WRAPPER(result_type, std_fname, std_wfname)   \
+  namespace aux_ {                                                            \
+    struct std_fname##_wrapper {                                              \
+      result_type                                                             \
+      operator()(const char * str, char ** endptr = 0, int base = 10) const { \
+        return static_cast<result_type>(::std::std_fname(str, endptr, base)); \
+      }                                                                       \
+                                                                              \
+      result_type                                                             \
+      operator()(const wchar_t * str, wchar_t ** endptr = 0,                  \
+                 int base = 10) const {                                       \
+        return static_cast<result_type>(                                      \
+          ::std::std_wfname(str, endptr, base));                              \
+      }                                                                       \
+    };                                                                        \
+  }
+
+#define FALCON_BASIC_CSTRING_TO_IMPL(result_type, char_type, fname, std_fname) \
+  template<typename Trait, typename Allocator>                                 \
   inline result_type                                                           \
-  fname(const std::basic_string<CharT, std::char_traits<CharT>, Allocator>& s, \
-        std::size_t* __idx = 0, int __base = 10)                               \
-  { return ::falcon::detail::stoa<result_type>                                 \
-  (&std_fname, #fname, s.c_str(), __idx, __base); }
+  fname(const std::basic_string<char_type, Trait, Allocator>& s,               \
+        std::size_t* idx = 0, int base = 10)                                   \
+  { return ::falcon::aux_::stoa<result_type>                                   \
+    (::falcon::aux_::std_fname##_wrapper(), #fname, s.c_str(), idx, base); }
 
-#define FALCON_BASIC_CSTRING_TO(result_type, fname, type)       \
-  FALCON_BASIC_CSTRING_TO_IMPL(result_type, fname, strto##type)
+#define FALCON_BASIC_CSTRING_TO(result_type, fname, type)                   \
+  FALCON_BASIC_CSTRING_TO_WRAPPER(result_type, strto##type, wcsto##type)    \
+  FALCON_BASIC_CSTRING_TO_IMPL(result_type, char, fname, strto##type)       \
+  FALCON_BASIC_CSTRING_TO_IMPL(result_type, const char, fname, strto##type) \
+  FALCON_BASIC_CSTRING_TO_IMPL(result_type, wchar_t, fname, strto##type)    \
+  FALCON_BASIC_CSTRING_TO_IMPL(result_type, const wchar_t, fname, strto##type)
 
-FALCON_BASIC_CSTRING_TO(int, stoi, l)
 FALCON_BASIC_CSTRING_TO(long, stol, l)
 FALCON_BASIC_CSTRING_TO(unsigned long, stoul, ul)
 #if __cplusplus >= 201103L
@@ -117,15 +92,34 @@ FALCON_BASIC_CSTRING_TO(long long, stoll, ll)
 FALCON_BASIC_CSTRING_TO(unsigned long long, stoull, ull)
 #endif
 
-#undef FALCON_BASIC_CSTRING_TO_IMPL
+# undef FALCON_BASIC_CSTRING_TO_WRAPPER
+#define FALCON_BASIC_CSTRING_TO_WRAPPER(result_type, std_fname, std_wfname)
+FALCON_BASIC_CSTRING_TO(int, stoi, l)
 
-#define FALCON_BASIC_CSTRING_TO_IMPL(result_type, fname, std_fname)            \
-  template<typename CharT, typename Allocator>                                 \
+# undef FALCON_BASIC_CSTRING_TO_WRAPPER
+#define FALCON_BASIC_CSTRING_TO_WRAPPER(result_type, std_fname, std_wfname) \
+  namespace aux_ {                                                          \
+    struct std_fname##_wrapper {                                            \
+      result_type                                                           \
+      operator()(const char * str, char ** endptr = 0) const {              \
+        return ::std::std_fname(str, endptr);                               \
+      }                                                                     \
+                                                                            \
+      result_type                                                           \
+      operator()(const wchar_t * str, wchar_t ** endptr = 0) const {        \
+        return ::std::std_wfname(str, endptr);                              \
+      }                                                                     \
+    };                                                                      \
+  }
+
+# undef FALCON_BASIC_CSTRING_TO_IMPL
+#define FALCON_BASIC_CSTRING_TO_IMPL(result_type, char_type, fname, std_fname) \
+  template<typename Trait, typename Allocator>                                 \
   inline result_type                                                           \
-  fname(const std::basic_string<CharT, std::char_traits<CharT>, Allocator>& s, \
-        std::size_t* __idx = 0)                                                \
-  { return ::falcon::detail::stoa<result_type>                                 \
-  (&std_fname, #fname, s.c_str(), __idx); }
+  fname(const std::basic_string<char_type, Trait, Allocator>& s,               \
+        std::size_t* idx = 0)                                                  \
+  { return ::falcon::aux_::stoa<result_type>                                   \
+    (::falcon::aux_::std_fname##_wrapper(), #fname, s.c_str(), idx); }
 
 FALCON_BASIC_CSTRING_TO(float, stof, f)
 FALCON_BASIC_CSTRING_TO(double, stod, d)
@@ -134,6 +128,7 @@ FALCON_BASIC_CSTRING_TO(double long, stold, ld)
 #endif
 #undef FALCON_BASIC_CSTRING_TO
 #undef FALCON_BASIC_CSTRING_TO_IMPL
+#undef FALCON_BASIC_CSTRING_TO_WRAPPER
 
 }
 
