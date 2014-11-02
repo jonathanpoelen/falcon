56e4da8d87f90e5a3bf4756febb96322adf05605
jonathan poelen <jonathan.poelen@gmail.com>
2014-11-02 22:35:34 +0100

    fix falcon::get

---
+++ falcon/tuple/get.hpp
@@ -0,0 +1,38 @@
+#ifndef FALCON_TUPLE_GET_HPP
+#define FALCON_TUPLE_GET_HPP
+
+#include <array>
+
+namespace falcon_tuple_get_impl {
+  using std::get;
+
+  template<std::size_t I, class T>
+  constexpr auto tuple_get_impl(T & a)
+  noexcept(noexcept(get<I>(a)))
+  -> decltype(get<I>(a))
+  { return get<I>(a); }
+
+  template<std::size_t I, class T>
+  constexpr auto tuple_get_impl(T && a)
+  noexcept(noexcept(get<I>(std::move(a))))
+  -> decltype(get<I>(std::move(a)))
+  { return get<I>(std::move(a)); }
+}
+
+namespace falcon {
+
+template<std::size_t I, class T>
+constexpr auto get(T& t)
+noexcept(noexcept(::falcon_tuple_get_impl::tuple_get_impl<I>(t)))
+-> decltype(::falcon_tuple_get_impl::tuple_get_impl<I>(t))
+{ return ::falcon_tuple_get_impl::tuple_get_impl<I>(t); }
+
+template<std::size_t I, class T>
+constexpr auto get(T&& t)
+noexcept(noexcept(::falcon_tuple_get_impl::tuple_get_impl<I>(std::move(t))))
+-> decltype(::falcon_tuple_get_impl::tuple_get_impl<I>(std::move(t)))
+{ return ::falcon_tuple_get_impl::tuple_get_impl<I>(std::move(t)); }
+
+}
+
+#endif
