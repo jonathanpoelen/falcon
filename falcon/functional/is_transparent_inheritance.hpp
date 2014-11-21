e73f5ef29786f6eb0f07f44edd05d1e9fa77a40c
jonathan poelen <jonathan.poelen@gmail.com>
2014-11-21 23:14:55 +0100

    cleanup source code

--- falcon/functional/is_transparent_inheritance.hpp
+++ falcon/functional/is_transparent_inheritance.hpp
@@ -1,6 +1,7 @@
 #ifndef FALCON_FUNCTIONAL_IS_TRANSPARENT_INHERITANCE_HPP
 #define FALCON_FUNCTIONAL_IS_TRANSPARENT_INHERITANCE_HPP
 
+#include <falcon/helper/restrict_is_transparent.hpp>
 #include <falcon/helper/has_is_transparent.hpp>
 #include <falcon/type_traits/use_if.hpp>
 #include <falcon/type_traits/dummy.hpp>
@@ -8,16 +9,10 @@
 
 namespace falcon {
 
-namespace aux_ {
-  template<class T>
-  struct propage_is_transparence
-  { typedef typename T::is_transparence is_transparence; };
-}
-
 template<class T>
 using is_transparent_inheritance = if_c<
   has_is_transparent<T>
-, aux_::propage_is_transparence<T>
+, restrict_is_transparent<T>
 , dummy
 >;
 
