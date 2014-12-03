a98f202cdc94c9e200b06db92311bea4be791fe1
jonathan poelen <jonathan.poelen@gmail.com>
2014-12-03 23:24:59 +0100

    transform_accumulate algorithm

---
+++ falcon/algorithm/transform_accumulate.hpp
@@ -0,0 +1,36 @@
+#ifndef FALCON_ALGORITHM_TRANSFORM_ACCUMULATE_HPP
+#define FALCON_ALGORITHM_TRANSFORM_ACCUMULATE_HPP
+
+namespace falcon {
+
+/// init = op(init, unary_op(*first))
+template<
+  class InputIt, class UnaryOperation
+, class T, class BinaryOperation>
+T transform_accumulate(
+  InputIt first, InputIt last, UnaryOperation unary_op
+, T init, BinaryOperation op)
+{
+  for (; first != last; ++first) {
+    init = op(init, unary_op(*first));
+  }
+  return init;
+}
+
+/// init = op(init, binary_op(*first1, *first2))
+template<
+  class InputIt1, class InputIt2, class BinaryOperation
+, class T, class BinaryOperationReduce>
+T transform_accumulate(
+  InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryOperation binary_op
+, T init, BinaryOperationReduce op)
+{
+  for (; first1 != last1; ++first1, ++first2) {
+    init = op(init, binary_op(*first1, *first2));
+  }
+  return init;
+}
+
+}
+
+#endif
